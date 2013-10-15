7e8a7cfd82c166b7f36c2d1b35feda226669c857
Charl Albre Sparks <charl_sparks@hotmail.com>
2013-10-16 12:05:53 +1300

    New Commits

---
+++ js/EmployeeView.js
@@ -0,0 +1,17 @@
+var EmployeeView = function() {
+ 
+
+    this.initialize = function() {
+        this.el = $('<div/>');
+    };
+ 
+    this.initialize();
+
+    this.render = function() {
+	    this.el.html(EmployeeView.template(employee));
+	    return this;
+	};
+ 
+}
+ 
+EmployeeView.template = Handlebars.compile($("#employee-tpl").html());
